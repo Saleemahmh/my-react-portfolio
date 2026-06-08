bd8f3ee2ac9bea4d00de2a81e0bcd44c93e0858e
Saleemahmh <ssaleemah24@gmail.com>
2026-06-08 13:13:41 +0530

    More projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,9 @@
 import literaryImg from "../assets/project/project2.png";
 import portfolioImg from "../assets/project/project4.png";
 import holidayImg from "../assets/project/project3.png";
+import myportfolioImg from "../assets/project/project.png";
+import switchtodubaiImg from "../assets/project/project5.png";
+import dashboardImg from "../assets/project/project6.png";
 export const projects = [
   {
     title: "Literary App",
@@ -16,13 +19,7 @@ export const projects = [
     imageSrc: portfolioImg,
     description:
       "This project is made using React Vite to learn and showcase my ability as a Developer",
-    skills: [
-      "ReactVite",
-      "Node",
-      "Tailwind CSS",
-      "framer Motion",
-      "ParticleJs",
-    ],
+    skills: ["Tailwind CSS", "Framer Motion", "ParticleJs"],
     demo: "https://samee-portfolio.vercel.app/",
     source: "https://github.com/Saleemahmh/samee-portfolio",
   },
@@ -34,5 +31,32 @@ export const projects = [
     skills: ["React", "Express", "Node", "MongoDB", "Sass"],
     demo: "https://www.example.com",
     source: "https://github.com/Saleemahmh/DreamHaven--Vacation-booking-app",
+  },
+  {
+    title: "My Portfolio Page",
+    imageSrc: myportfolioImg,
+    description:
+      "This project is made using basics of React Js with CSS to have in-depth knowledge of them. ",
+    skills: ["Javascript", "React", "CSS"],
+    demo: "https://my-react-portfolio-delta-five.vercel.app/",
+    source: "https://github.com/Saleemahmh/my-react-portfolio",
+  },
+  {
+    title: "Business Visa Page",
+    imageSrc: switchtodubaiImg,
+    description:
+      "This application is build using React + Vite framework making it lightweight and with minimal setup. ",
+    skills: ["Tailwind CSS", "Framer Motion", "React-icons"],
+    demo: "https://client-two-neon-21.vercel.app/",
+    source: "https://github.com/Saleemahmh/client",
+  },
+  {
+    title: "Health Dashboard",
+    imageSrc: dashboardImg,
+    description:
+      "This is a project made to learn Mern stack Javascript languages by building an ecommerce portal.",
+    skills: ["React", "Recharts", "Node", "TailwindCSS"],
+    demo: "https://graphdashboard.netlify.app/",
+    source: "https://github.com/Saleemahmh/dashboard_widgets",
   },
 ];
